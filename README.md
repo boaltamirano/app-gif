ad904f9d1e67c6a1723bb84678527473ca1d7339
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-04-12 22:57:43 -0500

    feat: Add options and validations in add

---
+++ README.md
@@ -0,0 +1,5 @@
+# To raise service in local
+
+* Access [Giphy Developers]('https://developers.giphy.com') and get an API_KEY
+
+
--- src/GifExpert.jsx
+++ src/GifExpert.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpert = () => {
-  return (
-    <>
-        <h1>Gif Expert App</h1> 
-    </>
-  )
+    const [categories, setCategories] = useState(['One', "Dragon"]);
+
+    const onAddCategory = (newCategory) =>{
+        if(categories.includes(newCategory)) return;
+        setCategories([...categories, newCategory]);
+    }
+
+    return (
+        <>
+            <h1>Gif Expert </h1> 
+            <AddCategory 
+                onNewCategory={ (value) => onAddCategory(value) }
+            />
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
+        </>
+    )
 }
 
